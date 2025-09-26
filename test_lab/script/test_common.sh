7ddee34fcb4995d2338246e3731b2fdd17f79353
WATERR <Raleigh.Waters@intelsat.com>
2025-09-26 15:24:08 -0400

    Refactor scripts and configs: update paths for test_res.sh, deprecate unused variables, and add new test_common.sh

--- test_lab/script/test_common.sh
+++ test_lab/script/test_common.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-export ICS_HOME=/opt/ICS
-
 ################################################################################
 # Global Variables
 ################################################################################
-#ICSSTART=${ICS_HOME}/bin/icsstart
-#ICSSTOP=${ICS_HOME}/bin/icsstop
-#ICSRES=${ICS_HOME}/bin/icsres
-#ICSGRP=${ICS_HOME}/bin/icsgrp
-#ICSSYS=${ICS_HOME}/bin/icssys
+RES_FILES=/app/data/res
+RES_SCRIPT=/app/script/test_res.sh
 
-#ICS_VAR=/var/opt/ics
-#ICS_CONFIG=${ICS_VAR}/config/main.cf
-
-RES_FILES=${ICS_VAR}/test_resources
-RES_SCRIPT=${ICS_HOME}/test/test_res.sh
+icsctl() {
+    python3 /app/ics/cli/icsctl.py "$@"
+}
+icsgrp() {
+    python3 /app/ics/cli/icsgrp.py "$@"
+}
+icsres() {
+    python3 /app/ics/cli/icsres.py "$@"
+}
 
 ################################################################################
 # Functions
